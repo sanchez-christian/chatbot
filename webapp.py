abcc707c76e16a7ca3e9af78d021379d98c4c20c
sanchez-christian <sanchezchristian.us@gmail.com>
2022-03-02 09:58:07 -0800

    End 3/2/22

--- webapp.py
+++ webapp.py
@@ -1,25 +1,61 @@
 #https://www.analyticsvidhya.com/blog/2021/12/creating-chatbot-building-using-python/
 from flask import Flask, url_for, render_template, request
+from chatterbot import ChatBot
+bot = ChatBot("cbot")
+bot = ChatBot(
+    'cbot',
+    storage_adapter='chatterbot.storage.SQLStorageAdapter',
+     logic_adapters=[
+        'chatterbot.logic.MathematicalEvaluation',
+        'chatterbot.logic.TimeLogicAdapter'
+    ],
+    database_uri='sqlite:///database.sqlite3'
+)
+#while True:
+#    try:
+#        bot_input = bot.get_response(input())
+#        print(bot_input)
+#
+#    except(KeyboardInterrupt, EOFError, SystemExit):
+#        break
+while True:
+    try:
+        user_input = input()
+
+        bot_response = bot.get_response(user_input)
+
+        print(bot_response)
+
+    # Press ctrl-c or ctrl-d on the keyboard to exit
+    except (KeyboardInterrupt, EOFError, SystemExit):
+        break
+from chatterbot.trainers import ListTrainer
+
+trainer = ListTrainer(bot)
+
+trainer.train([
+    "Hi! Would you like to order?",
+    "Of course, I would be happy to help process your order! What is your name?",
+    "#NAME, Would you like to see the menu?",
+    "What would you like to order? Please state the name or number.",
+    "Will you be paying online or in-store?",
+    "Great, #NAME!"
+])
 app = Flask(__name__) #__name__ = "__main__" if this is the file that was run.  Otherwise, it is the name of the file (ex. webapp)
 @app.route("/")
 def render_main():
     return render_template('home.html')
-@app.route("/response")
+@app.route("/text")
 def render_response():
-    color = request.args['color']
+    return render_template('home.html')
+
     #The request object stores information about the request sent to the server.
     #args is an ImmutableMultiDict (like a dictionary but can have mutliple values for the same key and can't be changed)
     #The information in args is visible in the url for the page being requested. ex. .../response?color=blue
-    if color == 'pink':
-        reply1 = "That's my favorite color, too!"
-    else:
-        reply1 = "My favorite color is pink."
-    n = request.args['multNum']
-    n = int(request.args['multNum']) #values in request.args are strings by default
-    reply2 = "2 x " + str(n) + " = " + str((2*n))
-    return render_template('response.html', response1 = reply1, response2 = reply2)
 
-if __name__=="__main__":
-    app.run(debug=False)
-print ("BOT: What is  your name?")
-user_name = input ()
+#if __name__=="__main__":
+#    app.run(debug=False)-->
+
+#chatbot = ChatBot('cbot')
+#print ("BOT: What is  your name?")
+# user_name = input ()
