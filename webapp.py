e76fb61a48e8b6eb7cf4dbc8dcb416582acb9b32
sanchez-christian <sanchezchristian.us@gmail.com>
2022-03-03 10:21:06 -0800

    End 3/3/22

--- webapp.py
+++ webapp.py
@@ -1,16 +1,28 @@
-#https://www.analyticsvidhya.com/blog/2021/12/creating-chatbot-building-using-python/
 from flask import Flask, url_for, render_template, request
 from chatterbot import ChatBot
-bot = ChatBot("cbot")
+from chatterbot.trainers import ListTrainer
 bot = ChatBot(
     'cbot',
     storage_adapter='chatterbot.storage.SQLStorageAdapter',
      logic_adapters=[
         'chatterbot.logic.MathematicalEvaluation',
-        'chatterbot.logic.TimeLogicAdapter'
+        'chatterbot.logic.BestMatch',
     ],
     database_uri='sqlite:///database.sqlite3'
 )
+
+print("Type something to begin...")
+
+trainer = ListTrainer(bot)
+
+trainer.train([
+    "Hi! Would you like to order?",
+    "Of course, I would be happy to help process your order! What is your name?",
+    "#NAME, Would you like to see the menu?",
+    "What would you like to order? Please state the name or number.",
+    "Will you be paying online or in-store?",
+    "Great, #NAME!"
+])
 #while True:
 #    try:
 #        bot_input = bot.get_response(input())
@@ -29,26 +41,6 @@ while True:
     # Press ctrl-c or ctrl-d on the keyboard to exit
     except (KeyboardInterrupt, EOFError, SystemExit):
         break
-from chatterbot.trainers import ListTrainer
-
-trainer = ListTrainer(bot)
-
-trainer.train([
-    "Hi! Would you like to order?",
-    "Of course, I would be happy to help process your order! What is your name?",
-    "#NAME, Would you like to see the menu?",
-    "What would you like to order? Please state the name or number.",
-    "Will you be paying online or in-store?",
-    "Great, #NAME!"
-])
-app = Flask(__name__) #__name__ = "__main__" if this is the file that was run.  Otherwise, it is the name of the file (ex. webapp)
-@app.route("/")
-def render_main():
-    return render_template('home.html')
-@app.route("/text")
-def render_response():
-    return render_template('home.html')
-
     #The request object stores information about the request sent to the server.
     #args is an ImmutableMultiDict (like a dictionary but can have mutliple values for the same key and can't be changed)
     #The information in args is visible in the url for the page being requested. ex. .../response?color=blue
