17f34d16e713cd9e80bf36c8a3869631d00e3dae
sanchez-christian <sanchezchristian.us@gmail.com>
2022-03-11 09:47:48 -0800

    End 3/11/22

--- webapp.py
+++ webapp.py
@@ -1,56 +1,14 @@
-from flask import Flask, url_for, render_template, request
-#from chatterbot import ChatBot
-#from chatterbot.trainers import ListTrainer
-#from chatterbot.trainers import ChatterBot CorpusTrainer
-bot = ChatBot(
-    'cbot',
-    storage_adapter='chatterbot.storage.SQLStorageAdapter',
-     logic_adapters=[
-        'chatterbot.logic.MathematicalEvaluation',
-        'chatterbot.logic.BestMatch',
-    ],
-    database_uri='sqlite:///database.sqlite3'
-)
+import nltk
+import nltk.stem.lancaster import lancaster.Stemmer
+stemmer = LancasterStemmer()
 
-trainer.train("chatterbot.corpus.english")
+import numpy
+import tflearn
+import tensorflow
+import random
+import json
 
-print("Type something to begin...")
+with open("intents.json") as file:
+    data = json.load(file)
 
-trainer = ListTrainer(bot)
-
-trainer.train([
-    "Hi! Would you like to order?",
-    "Of course, I would be happy to help process your order! What is your name?",
-    "#NAME, Would you like to see the menu?",
-    "What would you like to order? Please state the name or number.",
-    "Will you be paying online or in-store?",
-    "Great, #NAME!"
-])
-#while True:
-#    try:
-#        bot_input = bot.get_response(input())
-#        print(bot_input)
-#
-#    except(KeyboardInterrupt, EOFError, SystemExit):
-#        break
-while True:
-    try:
-        user_input = input('You: ')
-
-        bot_response = bot.get_response(user_input)
-
-        print('Bot Name: ', bot_response)
-
-    # Press ctrl-c or ctrl-d on the keyboard to exit
-    except (KeyboardInterrupt, EOFError, SystemExit):
-        break
-    #The request object stores information about the request sent to the server.
-    #args is an ImmutableMultiDict (like a dictionary but can have mutliple values for the same key and can't be changed)
-    #The information in args is visible in the url for the page being requested. ex. .../response?color=blue
-
-#if __name__=="__main__":
-#    app.run(debug=False)-->
-
-#chatbot = ChatBot('cbot')
-#print ("BOT: What is  your name?")
-# user_name = input ()
+print(data)
