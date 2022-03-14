a3fe2cd610deb791af2e9b76bbe68bcf1690956a
sanchez-christian <sanchezchristian.us@gmail.com>
2022-03-14 09:56:53 -0700

    End 3/14/22

--- webapp.py
+++ webapp.py
@@ -1,5 +1,5 @@
 import nltk
-import nltk.stem.lancaster import lancaster.Stemmer
+from nltk.stem.lancaster import LancasterStemmer
 stemmer = LancasterStemmer()
 
 import numpy
@@ -11,4 +11,20 @@ import json
 with open("intents.json") as file:
     data = json.load(file)
 
-print(data)
+words = []
+labels = []
+docs_x = []
+docs_y = []
+
+for intent in data["intents"]:
+    for pattern in intent["patterns"]:
+        wrds = nltk.word_tokenize(pattern)
+        words.extend(wrds)
+        docs_x.append(pattern)
+        docs_y.append(intent["tag"])
+
+    if intent["tag"] not in labels:
+        labels.append(intent["tag"])
+
+words = [stemmer.stem(w.lower()) for w in words]
+words = sorted(list(set(words)))
