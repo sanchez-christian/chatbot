78c2d1a7b235ce4decf220777ae94789a5c26677
sanchez-christian <sanchezchristian.us@gmail.com>
2022-03-17 10:26:57 -0700

    End 3/17/22

--- webapp.py
+++ webapp.py
@@ -53,5 +53,19 @@ for x, doc in enumerate(docs_x):
     training.append(bag)
     output.append(output_row)
 
+
 training = numpy.array(training)
 output = numpy.array(output)
+
+tensorflow.reset_default_graph()
+
+net = tflearn.input_data(shape=[None, len(training[0])])
+net = tflearn.fully_connected(net, 8) #This is a hidden layer. With more and more intents or tags in json file, you want to add more neurons to your hidden layers but 2 hidden layers is typically enough for a problem like this! Refer to Part 3 starting at 5:27 timestamp.
+net = tflearn.fully_connected(net, 8) #This is a hidden layer.
+net = tflearn.fully_connected(net, len(output[0]), activation="softmax")
+net = tflearn.regression(net)
+
+model = tflearn.DNN(net)
+
+model.fit(training, output, n_epoch=1000, batch_size=8, show_metric=True) #n_epoch=number is the amount of times that it's going to see the same data. Here we're gonna show it the same data 1000 times, but adjust to 2000,5000,100, etc. as seen fit. The more it sees the data, the better it gets a classifying. Refer to Part starting at 10:25 timestamp.
+model.save("model.tflearn")
